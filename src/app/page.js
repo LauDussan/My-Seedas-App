9cb07c31a7a4a9c094d619be1466cb6f519eb330
calossandres <163074147+calossandres@users.noreply.github.com>
2024-04-24 08:08:25 -0500

    color de la pagina

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
 import styles from "./page.module.css";
-import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
