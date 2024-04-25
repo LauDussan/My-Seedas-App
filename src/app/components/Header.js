fa4f52f1ae1e98b88b66383ad13deb6ed1903ede
calossandres <163074147+calossandres@users.noreply.github.com>
2024-04-24 22:40:01 -0500

    logo

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,8 +1,38 @@
 import React from 'react'
+import Image from 'next/image'
 
 function Header(){
+    const headerMenu=[
+        {
+            id:1,
+            name:'trasporta',
+            icon:'/logo-carro.jpg'
+        },
+        {
+            id:2,
+            name:'usuario',
+            icon:'/logo-usuario.jpg'
+        }
+    ]
     return (
-        <div> Header </div>
+        <div className='p-5 pb-3 pl-10 border-b-[4px] 
+        border-red-200'> 
+            <div className='flex gap-24 items-center'>
+                <Image src='/LOGO-SEEDAS.jpg'
+                width={70} height={100}
+                alt='logo'/>
+                <div className='flex gap-6 items-center'>     
+                    {headerMenu.map((item)=>(
+                        <div className='flex gap-2 items-center'>
+                            <Image src={item.icon}
+                            width={17} height={17}/>
+                            <h2 className='text-[14px] font-medium'>{item.name}</h2>
+                        </div>    
+                ))} 
+
+                </div>
+            </div> 
+        </div>
     )
 }
 
